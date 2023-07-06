8b3e498bf6554f73a733774a5880e21ebf5d640a
utkn <39ph4f87a@gmail.com>
2023-07-06 15:21:58 +0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # h_mat
-A convenient heterogenous matrix type in Rust.
+A type-safe and convenient heterogenous matrix type in Rust. Intended to use for an ECS with compile-time type-checking. 
+
+### Creation and basic row access
+```rust
+// Creating a HMat with i32, f32, usize rows.
+let mat = h_mat::HMat::new::<usize>().extend::<f32>().extend::<i32>();
+// Access the rows explicitly as a reference.
+let usize_row_ref: &Row<usize> = mat.get_row_ref();
+let i32_row_ref: &Row<i32> = mat.get_row_ref();
+// ... or as a mutable reference.
+let mut mat = mat;
+let i32_row_mut: &mut Row<i32> = mat.get_row_mut();
+```
+
+### Reforming
+```rust
+// Invoke `reform` to extract a reference matrix with arbitrary row order.
+// The returned type `HMatRef` is a heterogenous matrix of reference rows.
+// Note: We need to bind by && in order to be able to `reform`. This will be fixed in the future.
+let mat = &&h_mat::HMat::new::<usize>().extend::<f32>().extend::<i32>();
+// Reform as a heterogenous matrix of f32, and i32 rows.
+let mat_ref: HMatRef<f32, HMatRef<i32, ()>> = mat.reform();
+// ... also works as an argument!
+fn receive_reformed(_: HMatRef<f32, HMatRef<i32, ()>>) {}
+receive_reformed(mat.reform());
+// Of course, we can access the rows of the original matrix.
+let i32_row_ref: &Row<i32> = mat_ref.get_row_ref();
+```
+
+### Column access
+```rust
+let mat = h_mat::HMat::new::<usize>().extend::<f32>().extend::<i32>();
+// Access a single column as a reference.
+let col_ref: HCol<&i32, HCol<&f32, HCol<&usize, ()>>> = mat.get_col_ref(0);
+// ... or as a mutable reference...
+let mut mat = mat;
+let col_mut: HCol<&mut i32, HCol<&mut f32, HCol<&mut usize, ()>>> = mat.get_col_mut(0);
+// ... or directly move it out of the matrix.
+let col: HCol<i32, HCol<f32, HCol<usize, ()>>> = mat.take_col(0);
+// Then we can place it back to a different position.
+mat.place_col(1, col);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,11 @@ mod tests {
     fn access_row() {
         let mat = h_mat::HMat::new::<usize>().extend::<f32>().extend::<i32>();
         // Access the rows explicitly as a reference.
-        let _usize_row: &Row<usize> = mat.get_row_ref();
-        let _i32_row: &Row<i32> = mat.get_row_ref();
+        let usize_row: &Row<usize> = mat.get_row_ref();
+        let i32_row: &Row<i32> = mat.get_row_ref();
         // ... or as a mutable reference.
         let mut mat = mat;
-        let _i32_row: &mut Row<i32> = mat.get_row_mut();
+        let i32_row_mut: &mut Row<i32> = mat.get_row_mut();
     }
 
     #[test]
@@ -33,8 +33,8 @@ mod tests {
         let ref_mat: HMatRef<f32, HMatRef<i32, ()>> =
             Reform::<f32, ReformDirective<i32, _, _>, _>::reform(mat);
         // Access the rows as a reference to the original matrix.
-        let _f32_row: &Row<f32> = ref_mat.get_row_ref();
-        let _i32_row: &Row<i32> = ref_mat.get_row_ref();
+        let f32_row: &Row<f32> = ref_mat.get_row_ref();
+        let i32_row: &Row<i32> = ref_mat.get_row_ref();
     }
 
     #[test]
@@ -48,5 +48,19 @@ mod tests {
         // ... also works as an argument!
         fn receive_reformed(_: HMatRef<f32, HMatRef<i32, ()>>) {}
         receive_reformed(mat.reform())
+    }
+
+    #[test]
+    fn access_col() {
+        let mat = h_mat::HMat::new::<usize>().extend::<f32>().extend::<i32>();
+        // Access a single column as a reference.
+        let col_ref: HCol<&i32, HCol<&f32, HCol<&usize, ()>>> = mat.get_col_ref(0);
+        // ... or as a mutable reference...
+        let mut mat = mat;
+        let col_mut: HCol<&mut i32, HCol<&mut f32, HCol<&mut usize, ()>>> = mat.get_col_mut(0);
+        // ... or directly move it out of the matrix.
+        let col: HCol<i32, HCol<f32, HCol<usize, ()>>> = mat.take_col(0);
+        // Then we can place it back to a different position.
+        mat.place_col(1, col);
     }
 }
